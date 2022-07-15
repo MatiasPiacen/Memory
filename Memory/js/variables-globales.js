d9983f0231eb4dfd21bdada8fa44c06e41346383
MatiasPiacen <103008153+MatiasPiacen@users.noreply.github.com>
2022-07-14 21:17:30 -0300

    Proyecto

---
+++ Memory/js/variables-globales.js
@@ -0,0 +1,10 @@
+//var grupoTarjetas = [
+//    ["Pokemons/1.jpg", "Pokemons/2.jpg"]
+//    ["Pokemons/3.jpg", "Pokemons/4.jpg"],
+//    ["Pokemons/5.jpg", "Pokemons/6.jpg", "./Pokemons/7.jpg", "./Pokemons/8.jpg"], 
+//    ["Pokemons/9.jpg", "Pokemons/10.jpg", "./Pokemons/11.jpg", "./Pokemons/12.jpg"], 
+//    ["Pokemons/13.jpg", "Pokemons/14.jpg", "./Pokemons/15.jpg", "./Pokemons/16.jpg"]
+//];
+
+
+var grupoTarjetas = ["./Pokemons/1.jpg", "./Pokemons/2.jpg", "./Pokemons/3.jpg", "./Pokemons/4.jpg", "./Pokemons/5.jpg", "./Pokemons/6.jpg", "./Pokemons/7.jpg", "./Pokemons/8.jpg", "./Pokemons/9.jpg", "./Pokemons/10.jpg", "./Pokemons/11.jpg", "./Pokemons/12.jpg", "./Pokemons/13.jpg", "./Pokemons/14.jpg", "./Pokemons/15.jpg", "./Pokemons/16.jpg",]
